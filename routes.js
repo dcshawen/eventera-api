a4829855f7e885158574922428fc8da745e9af56
Dan Shaw <dcshaw.en@gmail.com>
2025-11-12 09:12:05 -0400

    Implements /events/id endpoint

--- routes.js
+++ routes.js
@@ -9,9 +9,32 @@ const dbConnectionString = process.env.DB_CONNECTION_STRING;
 router.get('/', async (req, res) => {
 	try {
 		await sql.connect(dbConnectionString);
-		const result = await sql.query`SELECT * FROM dbo.AstronomicalEvent`;
+		const result = await sql.query`SELECT * FROM dbo.AstronomicalEvent
+			INNER JOIN dbo.Category ON dbo.AstronomicalEvent.CategoryID = dbo.Category.CategoryID
+			ORDER BY dbo.AstronomicalEvent.StartDateTime DESC`;
 		console.dir(result);
 		res.json(result.recordset);
+	} catch (err) {
+		res.status(500).send('Database query failed');
+	}
+});
+
+// GET: /api/events/:id
+router.get('/:id', async (req, res) => {
+	const eventId = req.params.id;
+
+	if (isNaN(eventId)) {
+		return res.status(400).send('Invalid event ID');
+	}
+
+	try {
+		await sql.connect(dbConnectionString);
+		const result = await sql.query`SELECT * FROM dbo.AstronomicalEvent WHERE AstronomicalEventID = ${eventId}`;
+		console.dir(result);
+		if (result.recordset.length === 0) {
+			return res.status(404).send('Event not found');
+		}
+		res.json(result.recordset[0]);
 	} catch (err) {
 		res.status(500).send('Database query failed');
 	}
